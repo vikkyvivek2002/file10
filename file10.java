5a18886d34643ce8bbeb920da68bb74b9481ea5f
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-08-25 00:31:54 +0530

    Add files via upload

---
+++ file10.java
@@ -0,0 +1,15 @@
+class A12 
+{
+	static int x = 10;
+	static void show()
+	{
+		System.out.println("value = "+ x);
+	}
+}
+public class file10
+{
+	public static void main(String[] args)
+	{
+		A12.show();
+	}
+}
